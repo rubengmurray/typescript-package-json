2c7dc0bffeb82b3ea9e037574fe888181cf7600e
Reece Daniels <rubengmurray@gmail.com>
2023-03-25 16:27:00 +0000

    local changes - drafts

--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,30 @@
 import * as vscode from 'vscode';
 import { updateTypeScriptVersion } from './versioning';
 
-
-// let mostRecentFile = '';
+// https://github.com/Microsoft/vscode/blob/c3b73d3733e579b984d765b4264e34031cbf4397/extensions/typescript-language-features/src/tsServer/versionManager.ts#L161
+// https://github.com/Microsoft/vscode/blob/c3b73d3733e579b984d765b4264e34031cbf4397/extensions/typescript-language-features/src/typescriptService.ts#L72
 
 /**
  * This method is called when then extension is activated (when vs code starts up)
  */
 export async function activate(_context: vscode.ExtensionContext) {
+  // const vsCodeTSLangExt = vscode.extensions.getExtension('vscode.typescript-language-features')?.activate()
+  // const vsCodeTSExt = vscode.extensions.getExtension('vscode.typescript')?.activate()
+
   // If the editor opens with a .ts file open, run the check against the file
-  if (vscode.window.activeTextEditor?.document.languageId === "typescript") {
-    // mostRecentFile = vscode.window.activeTextEditor?.document.fileName;
-    updateTypeScriptVersion(vscode.window.activeTextEditor?.document.fileName);
+  if (vscode.window.activeTextEditor?.document?.languageId === "typescript") {
+    updateTypeScriptVersion(vscode.window.activeTextEditor?.document?.fileName);
   }
 
   // Add a listener for all other opened files in the editor
   vscode.workspace.onDidOpenTextDocument((doc) => {
-    if (doc.languageId === "typescript") {
-      // mostRecentFile = doc.fileName;
+    if (doc?.languageId === "typescript") {
       updateTypeScriptVersion(doc.fileName);
     }
   });
 
   vscode.window.onDidChangeActiveTextEditor(doc => {
-    console.log('change focus');
-    if (doc?.document.languageId === "typescript") {
-      // mostRecentFile = doc.fileName;
+    if (doc?.document?.languageId === "typescript") {
       updateTypeScriptVersion(doc?.document.fileName);
     }
   });
